02929ba9a5cd32b83231bbdc7de9c1379adc98ac
tehnarka <primat.kpi@gmail.com>
2021-04-16 19:10:37 +0300

    BinTree

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include <iostream>
+
+TEST(destroy_tree, TestDel) {
+  EXPECT_EQ(1, 10, 25);
+  EXPECT_TRUE(NULL);
+}
+
+TEST(preorder_print, Testpr) {
+	EXPECT_EQ(1, 10, 25, 8);
+	EXPECT_TRUE(1, 10, 25, 8);
+}
